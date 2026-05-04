0e904759d05c690b77d535ecd728618fbbf4da71
MohammadReza Sakhaei <sakhaei.wd@gmail.com>
2026-05-04 03:32:31 +0330

    remove build artifacts from tracking

--- ResourceReservation.Core/obj/Debug/net9.0/ResourceReservation.Core.AssemblyInfo.cs
+++ ResourceReservation.Core/obj/Debug/net9.0/ResourceReservation.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ResourceReservation.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55489d7103123950ab7fa44010e64f3011622fea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de3a74089cbf81d56e9c5707637920c3555b21c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ResourceReservation.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ResourceReservation.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
